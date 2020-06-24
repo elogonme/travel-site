f6adaaa52b79d4de2ef1a4b23659ecfc9a7493f8
Eldar <elogon@gmail.com>
2020-06-24 13:10:34 +0400

    Updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,7 +1,7 @@
 exports.handler = function(event, context, callback) {
     const secretContent = `
     <h3>Welcome to The Secret Area</h3>
-    <p>Here we can tell you that the sky is <strong>blue<strong></p>`
+    <p>Here we can tell you that the sky is <strong>blue</strong></p>`
 
     let body
 
