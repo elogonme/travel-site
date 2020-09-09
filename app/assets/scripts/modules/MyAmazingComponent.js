cfa047870789d8c65062f6b17cf6daf9515a2bff
Eldar <elogon@gmail.com>
2020-09-09 10:37:34 -0400

    Changed react element styling

--- app/assets/scripts/modules/MyAmazingComponent.js
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -4,7 +4,7 @@ function MyAmazingComponent() {
     return (
         <div>
             <h1 className="section-title section-title--blue">This is My Amazing React Component</h1>
-            <p>React is great!</p>
+            <p class="site-footer">React is great!</p>
         </div>
     )
 }
